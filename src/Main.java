2499bdae58dba3bdd1323610a46f059d79d8f394
Divya Singh <divyasingh7035gmail.com>
2022-11-16 14:20:58 +0530

    Added Songs Application

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Album album1 = new Album("album1", "Alan Walker");
+        album1.addSong("Alone", 5.2);
+        album1.addSong("Faded" ,  4.2);
+        album1.addSong("DarkSide",3.5);
+
+        LinkedList<Song> myPlaylist = new LinkedList<>();
+        album1.addToPlayList("Alone",myPlaylist);
+        album1.addToPlayList("DarkSide",myPlaylist);
+
+        play(myPlaylist);
+    }
+
+    public static void play(LinkedList<Song>playList)
+    {
+        Scanner sc = new Scanner (System.in);
+        ListIterator<Song> itr = playList.listIterator();
+        if(!itr.hasNext())
+        {
+            System.out.println("playList is empty");
+            return;
+        }
+        System.out.println("You are now listening" + itr.next());
+        showMenu();
+
+        while(true)
+        {
+            int option = sc.nextInt();
+            switch(option)
+            {
+                case 0:
+                    System.out.println("Thank You for listening");
+                    return;
+
+                case 1:
+                    showMenu();
+                    break;
+
+                case 2:
+                    printList(playList);
+                    break;
+
+            }
+        }
+    }
+
+    public static void printList(LinkedList<Song>playList){
+        ListIterator<Song>itr = playList.listIterator();
+        while(itr.hasNext())
+        {
+            System.out.println(itr.next());
+        }
+        return;
+    }
+
+    public static void showMenu(){
+        System.out.println("0.Exit");
+        System.out.println("1. Print Menu");
+        System.out.println("2. Show the list of all songs in the playlist");
+        System.out.println("3. Play next song");
+        System.out.println("4. Play previous song");
+        System.out.println("5. repeat the song");
+        System.out.println(("6. Delete the song"));
+    }
+}
